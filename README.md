0cf7a4d8add13ecee733fbbb6b006dff610e80e4
Marcos Lucas <marcosvaldeni@gmail.com>
2021-03-19 19:23:58 +0000

    project completed

---
+++ README.md
@@ -0,0 +1,82 @@
+# STOCKSHOES
+
+> “Education is the most powerful weapon which you can use to change the world.” Nelson Mandela
+
+<br />
+<p align="center"><img src=".github/stockshoes.gif?raw=true"/></p>
+
+---
+
+# :pushpin: Table of Contents
+* [Description](#memo-description)
+* [Made With](#rocket-made-with)
+* [Installation](#construction_worker-installation)
+* [Getting Started](#runner-getting-started)
+* [Found a bug? Missing a specific feature?](#bug-issues)
+* [Contributing](#tada-contributing)
+* [License](#closed_book-license)
+
+# :memo: Description
+The project consists of a shopping cart build with React. When the project is executed, it retracts data from a simulated API (JSON-Server) to populate the home. Using the functionality called "useContext" the data is passed from the menu to the adjacent components.
+
+# :rocket: Made With
+
+* 💠 TypeScript
+* 💫 React
+* 🏷 useContext
+* 💾 JSON-Server
+* 💅🏻 Styled Components
+* 💅🏻 CSS
+* ✅ React Toastify
+
+# :construction_worker: Installation
+
+**You need to install at least [NodeJS 12.18.2](https://nodejs.org/) and [Yarn](https://classic.yarnpkg.com/en/docs/install/), then in order to clone the project via HTTPS, run this command:**
+
+```git clone https://github.com/marcosvaldeni/stockshoes.git```
+
+SSH URLs provide access to a Git repository via SSH, a secure protocol. If you have an SSH key registered in your Github account, clone the project using this command:
+
+```git clone github.com/marcosvaldeni/stockshoes.git```
+
+**Install Dependencies**
+
+In the project directory, you have to run:
+
+`yarn`
+
+This command will install all dependencies. It may take a while.
+
+# :runner: Getting Started
+
+**Starting**
+
+In the project directory, you can run:
+
+`yarn server`
+This will run the Fake API.
+
+In another terminal, you can run:
+`yarn start`
+This will run project.
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+# :bug: Issues
+
+Feel free to **file a new issue** with a respective title and description on the [Stockshoes](https://github.com/marcosvaldeni/stockshoes/issues) repository. If you already found a solution to your problem, **I would love to review your pull request**! Have a look at our [contribution guidelines](https://github.com/marcosvaldeni/stockshoes/blob/master/CONTRIBUTING.md) to find out about the coding standards.
+
+# :tada: Contributing
+
+Check out the [contributing](https://github.com/marcosvaldeni/stockshoes/blob/master/CONTRIBUTING.md) page to see the best places to file issues, start discussions and begin contributing.
+
+# :closed_book: License
+
+Released in 2021.
+This project is under the [MIT license](https://github.com/marcosvaldeni/stockshoes/blob/master/LICENSE).
+
+Made with love by [Marcos Lucas](https://github.com/marcosvaldeni) 💚🚀
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,28 +23,55 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {      
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
-    } catch {
-      // TODO
+
+      const found = cart.find(product => product.id === productId);
+
+      if (found) {
+        await updateProductAmount({ productId, amount: found.amount + 1});
+        return;
+      }    
+
+      const response = await api.get<Product>(`/products/${productId}`)
+      .catch(() => {
+        throw new Error("Erro na adição do produto");
+      });
+
+      const newCart = [...cart, { ...response.data, amount: 1 }];
+
+      localStorage.setItem("@RocketShoes:cart", JSON.stringify(newCart));
+
+      setCart(newCart);
+      toast.success("Produto adicionado ao carrinho");
+    } catch (err) {
+      toast.error(err.message);
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
-    } catch {
-      // TODO
+      const found = cart.find((product) => product.id === productId);
+
+      if (!found) {
+        throw new Error('Erro na remoção do produto');
+      }
+      const newCart = cart.filter(task => task.id !== productId);
+
+      localStorage.setItem("@RocketShoes:cart", JSON.stringify(newCart));
+      setCart(newCart);
+      toast.success("Produto removido do carrinho");
+    } catch (err) {
+      toast.error(err.message);
     }
   };
 
@@ -53,9 +80,36 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
-    } catch {
-      // TODO
+
+      const found = cart.find((product) => product.id === productId);
+
+      if (!found || amount === 0) {
+        throw new Error("Erro na alteração de quantidade do produto");
+      }
+
+      const response = await api.get<Stock>(`/stock/${productId}`);
+
+      if (found) {
+        if (response.data.amount < amount) {
+          throw new Error("Quantidade solicitada fora de estoque");
+        }
+
+        const newCart = cart.map(product => {
+          if (product.id === productId) {
+            product.amount = amount;
+          }
+          return product;
+        });
+
+        localStorage.setItem("@RocketShoes:cart", JSON.stringify(newCart));
+        setCart(newCart);
+        toast.success("Produto adicionado ao carrinho");
+        return;
+      }
+
+      throw new Error("Erro na alteração de quantidade do produto");
+    } catch (err) {
+      toast.error(err.message);
     }
   };
 
